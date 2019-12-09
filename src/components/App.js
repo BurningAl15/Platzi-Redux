d9f2c93e451dac69e7d9c6a3bfb18c7667a67640
Aldhair Vera <aldhairvera15@gmail.com>
2019-12-08 21:36:13 -0500

    class 10 - Adding React Router Dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,18 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
-//Stateless (componentes no funcionales)
-/*Diferencias
-No Funcionales no manejan estados, solo manejan información o funciones 
-const App =()=>{ }
-*/
+import Usuarios from "./Usuarios/index";
+import Menu from "./Menu";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      usuarios: []
-    };
-  }
+const Tareas = () => <div>Tareas</div>;
 
-  async componentDidMount() {
-    const respuesta = await axios.get("https://jsonplaceholder.typicode.com/users");
-    
-    this.setState({
-      usuarios: respuesta.data
-    });
-  }
+const App = () => (
+  <BrowserRouter>
+    <Menu />
+    <Route exact path="/" component={Usuarios} />
 
-  ponerFilas = () =>
-    //Map itera por la cantidad de elementos del arreglo
-    this.state.usuarios.map((usuario) => (
-      <tr key={usuario.id}>
-        <td>{usuario.name}</td>
-        <td>{usuario.email}</td>
-        <td>{usuario.website}</td>
-      </tr>
-    ));
-
-  render() {
-    return (
-      <div className="margen">
-        <table className="tabla">
-          <thead>
-            <tr>
-              <th>Nombre</th>
-              <th>Correo</th>
-              <th>Enlace</th>
-            </tr>
-          </thead>
-          <tbody>{this.ponerFilas()}</tbody>
-        </table>
-      </div>
-    );
-  }
-}
+    <Route exact path="/tareas" component={Tareas} />
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/components/Usuarios/index.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+//Stateless (componentes no funcionales)
+/*Diferencias
+No Funcionales no manejan estados, solo manejan información o funciones 
+const Usuarios =()=>{ }
+*/
+
+export default class Usuarios extends Component {
+  constructor() {
+    super();
+    this.state = {
+      usuarios: []
+    };
+  }
+
+  async componentDidMount() {
+    const respuesta = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+
+    this.setState({
+      usuarios: respuesta.data
+    });
+  }
+
+  ponerFilas = () =>
+    //Map itera por la cantidad de elementos del arreglo
+    this.state.usuarios.map(usuario => (
+      <tr key={usuario.id}>
+        <td>{usuario.name}</td>
+        <td>{usuario.email}</td>
+        <td>{usuario.website}</td>
+      </tr>
+    ));
+
+  render() {
+    return (
+      <div className="margen">
+        <table className="tabla">
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Correo</th>
+              <th>Enlace</th>
+            </tr>
+          </thead>
+          <tbody>{this.ponerFilas()}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
